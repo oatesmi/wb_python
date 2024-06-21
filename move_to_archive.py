7c3fd0a79e829849e88c25321d15759d9845d300
Morgan <oatesmi.74@gmail.com>
2024-06-21 14:34:35 -0500

    Add files via upload

--- move_to_archive.py
+++ move_to_archive.py
@@ -5,13 +5,32 @@ from pathlib import Path
 
 provisioning = "/Users/morgan.oates/Desktop/provisioning/"
 filepath = os.listdir(provisioning)
+
+pythonf = "/Users/morgan.oates/Desktop/python/"
+filepath_py = os.listdir(pythonf)
+
 now = str(datetime.datetime.now())
 
-if os.path.exists(filepath):
-    for files in filepath:
-        files = filepath + files
-       
-        if ".xlsx" in files:
-            newfile = Path(files).stem
-            os.rename(files, newfile + "_" + now + ".xlsx")
-            shutil.move(newfile + "_" + now + ".xlsx", filepath + "archive")
+if os.path.exists(provisioning):
+    counter = 0
+    while counter <= len(filepath):
+        for files in filepath:
+            files = provisioning + files
+            counter += 1
+
+            if ".xlsx" in files:
+                newfile = Path(files).stem
+                os.rename(files, newfile + "_" + now + ".xlsx")
+                shutil.move(newfile + "_" + now + ".xlsx", provisioning + "archive")
+
+if os.path.exists(pythonf):
+    counter = 0
+    while counter <= len(filepath_py):
+        for files in filepath_py:
+            files = pythonf + files
+            counter += 1
+            
+            if ".xlsx" in files:
+                newfile = Path(files).stem
+                os.rename(files, newfile + "_" + now + ".xlsx")
+                shutil.move(newfile + "_" + now + ".xlsx", pythonf + "archive")
